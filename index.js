8417a9ba7104148df1982bf8933331d31312a32f
bennymathew <benny.mathew@student.moringaschool.com>
2023-10-03 11:31:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+const num1 = 31; 
+const num2 = 2;
+const multiply = num1 * num2;
+
+// generates a random integer greater than 0
+const random = Math.floor(Math.random()*10)
+
+// calculates the remainder = 4
+const num3 = 39;
+const num4 = 7;
+const mod = num3 % num4; 
+
+// 20 as the highest number in the set
+const numbers = [10, 20, 15, 6, 17];
+const max = Math.max(...numbers); 
